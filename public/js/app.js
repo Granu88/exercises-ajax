7deabf9413820da678caeff89fd2fc072e40adc8
David VALLON <davidvallon@hotmail.fr>
2019-10-30 11:54:28 +0100

    Exo 3

--- public/js/app.js
+++ public/js/app.js
@@ -13,20 +13,37 @@ $(function() {
        }
     });
 
+    setTimeout(function () {
+      $.ajax({
+         url : 'ajax/delay5.html', // La ressource ciblée
+         type : 'GET', // Le type de la requête HTTP.
+         success: function(html) {
+           var element = $('delay')
+           $('#delay').html(html)
+           console.log('#delay');
+         },
+         error: function() {
+           console.log('error');
+         }
+      });
+    }, 5000)
+
+
+
     $.ajax({
-       url : 'ajax/delay5.html', // La ressource ciblée
+       url : 'ajax/info.html', // La ressource ciblée
        type : 'GET', // Le type de la requête HTTP.
        success: function(html) {
-         var element = $('delay')
-         $('#delay').html(html)
-         console.log('#delay');
+         var element = $('info')
+         $('#btn').click(function() {
+           $('#info').html(html)
+          console.log('#info');
+        });
        },
        error: function() {
          console.log('error');
        }
     });
-
-  
 
 
 
